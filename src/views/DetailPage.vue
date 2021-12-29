2a6423817b4385e29e97e3bc082b50836dcd958c
26 <muongkimhong086@gmail.com>
2021-12-29 23:50:12 +1100

    added detail page path

---
+++ src/views/DetailPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <span id="title">
+        <i>Hotels name is simply dummy text of the printing</i>
+      </span>
+    </div>
+
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailPage',
+}
+</script>
+
+<style scoped>
+#title {
+  font-family: 'Roboto', sans-serif;
+  font-size: 26px;
+}
+</style>
