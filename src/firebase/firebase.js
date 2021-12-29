d47f478fb4fd949e19ce53dd57c73c530387b6d3
Kimheng <Kimhengleang45@gmail.com>
2021-12-29 15:20:06 +0700

    Test git

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,6 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-import Filter from 'bad-words'
 import { ref, onUnmounted, computed } from 'vue'
 
 firebase.initializeApp({
