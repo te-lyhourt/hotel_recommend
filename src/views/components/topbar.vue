2d392dc06ff468bbf0458370988f37d7e2fbc935
te-lyhourt <74485952+te-lyhourt@users.noreply.github.com>
2022-01-25 12:44:49 +0700

    edit logo

--- src/views/components/topbar.vue
+++ src/views/components/topbar.vue
@@ -26,7 +26,7 @@
   </div>
 </template>
 <script>
-import logo from "../../assets/detail_page/login-logo.jpg";
+import logo from "../../assets/loginlogo.png";
 import SearchBar from "./searchBar.vue";
 import profile from "./profile.vue";
 import userAuth from "../../firebase/auth";
